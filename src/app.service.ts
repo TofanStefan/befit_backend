a58a7992c3a0b4681cf133ece6e3bead266c68af
Tofan Stefan <stefan.tofan@assist.ro>
2022-10-27 18:49:37 +0300

    TEst

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hey Paduraru!';
+    return 'Hey Paduraru, how are you !';
   }
 }
